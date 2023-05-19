bd3c76d48a1cb8bb224dcd849744b724339c8ad7
DESKTOP-7L80PGH\forcewave <jgsj@forcewave.co.kr>
2023-05-19 10:44:05 +0900

    08 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
-  <div class="name">{{name}}</div>
-  <button class="btn btn-primary" v-on:click="updateName">Click</button>
+  <div :class="nameClass">{{name}}</div>
+  <input :type="type" :value="name"/>
+  <button class="btn btn-primary" @click="updateName">Click</button>
 </template>
 
 <script>
   import { ref } from 'vue'; 
   export default {
     setup(){
-      const name = ref({
-        id : 1
-      });
+      const name = ref('Kossie');
+      const type = ref('number');
+      const nameClass = ref('');
 
       const updateName=()=>{
-        name.value.id = 2;
+        name.value = 'Coder';
+        type.value = 'text';
+        nameClass.value = 'name';
       }; 
 
       return {
         name
         , updateName
+        , type
+        , nameClass
       };
     }
   }
