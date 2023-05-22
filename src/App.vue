77af049685250e4df13ba64c61c1477d153a648f
DESKTOP-7L80PGH\forcewave <jgsj@forcewave.co.kr>
2023-05-22 10:11:01 +0900

    16 배열에서 아이템 삭제

--- src/App.vue
+++ src/App.vue
@@ -22,9 +22,14 @@
       </div>
       <div v-show="hasError" style="color:red;">this field cannot be empty</div>
     </form>
-    <div v-for="todo in todos" :key="todo.id" class="card mt-2">
-      <div class="card-body p-2">
-        <div class="form-check">
+    
+    <div v-if="!todos.length">
+      추가된 Todo가 없습니다.
+    </div>
+
+    <div v-for="(todo, index) in todos" :key="todo.id" class="card mt-2">
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
           <input 
             class="form-check-input" 
             type="checkbox" 
@@ -35,8 +40,12 @@
             {{todo.subject}}
           </label>
         </div>
+        <div>
+          <button @click="deleteTodo(index)" class="btn btn-danger">Delete</button>
+        </div>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -62,11 +71,16 @@
         }
       }; 
 
+      const deleteTodo =(index)=>{
+        todos.value.splice(index, 1);
+      }
+
       return {
         todo
         , onSubmit
         , todos
         , hasError
+        , deleteTodo
       };
     }
   }
