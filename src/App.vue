fe4d908a7717aecaf2c8bbff8142db133603121d
DESKTOP-7L80PGH\forcewave <jgsj@forcewave.co.kr>
2023-05-22 15:15:50 +0900

    24 검색 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="container">
-    <h4>Count {{count}}</h4>
-    <h4>double Count Computed {{doubleCountComputed}}</h4>
-    <h4>double Count Computed {{doubleCountComputed}}</h4>
-    <h4>double Count Method {{doubleCountMethod()}}</h4>
-    <h4>double Count Method {{doubleCountMethod()}}</h4>
-    <button @click="count++"> Add One </button>
-
     <h2>To-Do List</h2>
     
-    <!-- 자식 컴포넌트의 이벤트를 부모 컴포넌트에 연결 -->
+    <input
+      class="form-control"
+      type="text" 
+      v-model="searchText"
+      placeholder="Search"
+    />
+  <hr>
     <TodoSimpleForm @add-todo="addTodo" />
     
     <div v-if="!todos.length">
-      추가된 Todo가 없습니다.
+      There is nothings to display
     </div>
 
-    <!-- 컴포넌트 :[prop명]="접근허용할 변수명" -->
-    <TodoList :todos="todos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo"/>
+    <TodoList 
+      :todos="filteredTodos" 
+      @toggle-todo="toggleTodo" 
+      @delete-todo="deleteTodo"/>
 
   </div>
 </template>
@@ -28,14 +29,14 @@
   import TodoList from './components/TodoList.vue';
 
   export default {
-    components : { //가져온 컴포넌트를 등록
+    components : { 
       TodoSimpleForm
       , TodoList
     },
     setup(){
-      const todos = ref([]); //다른 컴포넌트에서도 사용하기 때문에 이동할 수 없음
+      const todos = ref([]);
 
-      const addTodo=(todo)=>{ //자식 컴포넌트가 emit하여 보내온 값을 받음
+      const addTodo=(todo)=>{
         todos.value.push(todo);
       }
 
@@ -47,25 +48,24 @@
         todos.value[index].completed = ! todos.value[index].completed
       }
 
-      const count = ref(1);
-      const doubleCountComputed = computed(()=>{
-        console.log('computed');
-        return count.value * 2;
-      })
+      const searchText = ref('');
+      const filteredTodos = computed(()=>{
+        if(searchText.value){
+          return todos.value.filter(todo =>{
+            return todo.subject.includes(searchText.value);
+          })
+        }
 
-      const doubleCountMethod=()=>{
-        console.log('method');
-        return count.value *2;
-      }
+        return todos.value;
+      })
 
       return {
         addTodo
         , todos
         , deleteTodo
         , toggleTodo
-        , count
-        , doubleCountComputed
-        , doubleCountMethod
+        , searchText
+        , filteredTodos
       };
     }
   }
