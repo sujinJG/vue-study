37cbb940674fa9127d5f4b9bd138bd880c0c6466
DESKTOP-7L80PGH\forcewave <jgsj@forcewave.co.kr>
2023-05-26 16:19:34 +0900

    49 To-Do 페이지 컴포넌트 만들기 2

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -2,24 +2,51 @@
   <h1>To-Do Page</h1>
   <div v-if="loading"> Loading... </div>
   <form v-else>
-    <div class="form-group">
-        <label> Todo Subject</label>
-        <input
-            v-model="todo.subject" 
-            type="text" class="form-control">
+    <div class="row">
+        <div class="col-6">
+            <div class="form-group">
+                <label> Todo Subject</label>
+                <input
+                    v-model="todo.subject" 
+                    type="text" class="form-control">
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="col-6">
+                <div class="form-group">
+                    <label> Status </label>
+                    <div>
+                        <button 
+                            type="button"
+                            class="btn"
+                            :class="todo.completed ? 'btn-success': 'btn-danger'"
+                            @click="toggleTodoStatus">
+                            {{todo.completed ? 'Completed' : 'Imcompleted' }}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">저장</button>
+        <button 
+            class="btn btn-outline-dark ml-2"
+            @click="moveToTodoListPage">
+            취소
+        </button>
     </div>
-    <button class="btn btn-primary">저장</button>    
   </form>
 </template>
 
 <script>
 import axios from 'axios';
-import {useRoute} from 'vue-router';
+import {useRoute, useRouter} from 'vue-router';
 import {ref} from '@vue/reactivity';
 
 export default {
     setup(){
         const route = useRoute();
+        const router = useRouter();
         const todo = ref(null);
         const loading = ref(true);
 
@@ -32,9 +59,21 @@ export default {
 
         getTodo();
 
+        const toggleTodoStatus =()=>{
+            todo.value.completed = !todo.value.completed;
+        }
+
+        const moveToTodoListPage=()=>{
+            router.push({
+                name: 'Todos'
+            })
+        }
+
         return {
             todo
             , loading
+            , toggleTodoStatus
+            , moveToTodoListPage
         }
         
         
