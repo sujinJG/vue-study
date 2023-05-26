98e4e7db19a57ef68c309f69d2012bba58e99d69
DESKTOP-7L80PGH\forcewave <jgsj@forcewave.co.kr>
2023-05-26 13:18:48 +0900

    42 Navbar 추가하기

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Kossie Coder</a>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">
+            Todos 
+            <span class="sr-only">(current)</span>
+          </a>
+        </li>
+      </ul>
+  </nav>
+
   <router-view />
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
