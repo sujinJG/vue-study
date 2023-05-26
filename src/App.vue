438ea14febd2ed2e74e806e8b740a80e9ab9d6fb
DESKTOP-7L80PGH\forcewave <jgsj@forcewave.co.kr>
2023-05-26 14:12:27 +0900

    43 router-link

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Kossie Coder</a>
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">
-            Todos 
-            <span class="sr-only">(current)</span>
-          </a>
-        </li>
-      </ul>
+    <router-link class="navbar-brand" to="/">Kossie Coder</router-link>
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link class="nav-link" to="/todos"> Todos  <span class="sr-only">(current)</span> </router-link>
+      </li>
+    </ul>
   </nav>
-
   <router-view />
 </template>
 
