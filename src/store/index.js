2b0bae24f3f0cf2dc526bfcbfa5048fcb5d39e48
DESKTOP-7L80PGH\forcewave <jgsj@forcewave.co.kr>
2023-05-31 15:41:56 +0900

    77 vuex mutations/actions

--- src/store/index.js
+++ src/store/index.js
@@ -6,5 +6,37 @@ export default createStore({
         toastAlertType :'',
         showToast : false,
         timeout : null
+    },
+    mutations: {
+        UPDATE_TOAST_MESSAGE(state, payload){
+            state.toastMessage = payload;
+        },
+        UPDATE_TOAST_ALERT_TYPE(state, payload){
+            state.toastAlertType = payload;
+        },
+        UPDATE_TOAST_STATUS(state, payload){
+            state.showToast = payload;
+        },
+        UPDATE_TOAST_TIMEOUT(state, payload){
+            state.timeout = payload;
+        }
+    },
+    actions: {
+        triggerToast({commit}, message, type='success') {
+            // toastMessage.value = message;
+            commit('UPDATE_TOAST_MESSAGE', message);
+            // toastAlertType.value = type;
+            commit('UPDATE_TOAST_ALERT_TYPE', type)
+
+            // toastAlertType.showToast = true;
+            commit('UPDATE_TOAST_STATUS', true);
+    
+            timeout.value = setTimeout(() => {
+                // toastMessage.value = "";
+                commit('UPDATE_TOAST_MESSAGE', '');
+                commit('UPDATE_TOAST_STATUS', false);
+                showToast.value = false;
+            }, 1500)
+        },
     }
 });
