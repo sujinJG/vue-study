4b50405f035019a92bd72511ef268a701b9c0c4a
DESKTOP-7L80PGH\forcewave <jgsj@forcewave.co.kr>
2023-05-31 12:48:06 +0900

    62 toRefs

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,20 @@
 <template>
   <div>Home Page</div>
+  <div>{{ count }}</div>
+  <button @click="count++">add</button>
 </template>
 
 <script>
+import {useCount} from '@/composables/count'
 export default {
+  setup(){
+    const {count} = useCount();  // 구조분해해서 접근할 경우 reactivity를 잃음
+    
 
+    return {
+      count
+    }
+  }
 }
 </script>
 
