e08a3d4b16330efcffb10ed221df10a210dfe72e
DESKTOP-7L80PGH\forcewave <jgsj@forcewave.co.kr>
2023-05-31 13:52:23 +0900

    75 vuex 설치하기

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; //index.js 는 생략 가능
+import store from './store';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
